362de220382b45df9cbafb47fbfcf03afea63c3b
Luiz Berto <diasbertoluiz@gmail.com>
2020-01-26 01:20:50 -0300

    Adiciona cors no backend.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const routes = require('./routes');
 
@@ -11,6 +12,7 @@ mongoose.connect('mongodb+srv://nodejs_app:Mp3xVUx5G3LF9huV@cluster0-nlt72.mongo
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
 app.listen(1337);
